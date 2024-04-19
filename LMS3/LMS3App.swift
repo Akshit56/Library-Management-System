f647331b2a93eaee35917ccb2a4b48666c2345e6
Aditya Majumdar <aa0298@srmist.edu.in>
2024-04-19 16:13:20 +0530

    Initial commit

---
+++ LMS3/LMS3App.swift
@@ -0,0 +1,17 @@
+//
+//  LMS3App.swift
+//  LMS3
+//
+//  Created by Aditya Majumdar on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LMS3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
